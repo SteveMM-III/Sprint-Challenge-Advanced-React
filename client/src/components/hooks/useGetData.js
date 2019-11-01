73fd184b4b4accf39231ebd32ca5f0fa9f60facc
Kronykus <MoreheadSteve@gmail.com>
2019-11-01 13:55:56 -0500

    eliminated test warning about useEffect in useGetData hook memory leak

--- client/src/components/hooks/useGetData.js
+++ client/src/components/hooks/useGetData.js
@@ -2,16 +2,43 @@ import { useEffect } from 'react';
 import axios from 'axios';
 
 import { useLocalStorage } from './useLocalStorage';
+import Axios from 'axios';
 
 export const useGetData = ( key, initialValue ) => {
   
   const [ value, setValue ] = useLocalStorage( key, initialValue );
 
   useEffect( () => {
+    const source = axios.CancelToken.source();
+
+    const loadData = async () => {
+      try {
+        const res = await Axios.get( 'http://localhost:5000/api/players', {
+          cancelToken: source.token
+        } );
+        setValue( res.data );
+      } catch ( err ) {
+        if ( Axios.isCancel( err ) ) {
+          // request cancelled
+        } else {
+          console.error( `Error: ${err}` );
+        }
+      }
+    };
+
+    loadData();
+/* 
     axios
       .get( 'http://localhost:5000/api/players' )
       .then( res => setValue( res.data ) )
       .catch( err => console.error( `Error: ${err}` ) );
+*/
+
+    console.log( 'useEffect' );
+
+    return () => {
+      source.cancel();
+    }
   }, [] ); 
   
 
